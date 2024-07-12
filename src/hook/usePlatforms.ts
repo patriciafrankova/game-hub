eddf741abde73b5e0c76bd13edd2a777136442bf
Patricia <frankova12@gmail.com>
2024-07-12 22:20:13 +0200

    Ship platforms with the app

--- src/hook/usePlatforms.ts
+++ src/hook/usePlatforms.ts
@@ -1,11 +1,5 @@
-import useData from "./useData";
+import platforms from "../data/platforms";
 
-interface Platform {
-  id: number;
-  slug: string;
-  name: string;
-}
-
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
 
 export default usePlatforms;
